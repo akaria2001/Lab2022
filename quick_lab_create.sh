cb4754e0704c641634cd311a60184e2cd179a602
Amit Karia <amit_karia2001@hotmail.com>
2023-07-16 19:51:19 +0100

    Add sleep to quick lab create shell script

--- quick_lab_create.sh
+++ quick_lab_create.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for f in $(cat file.toml | grep -i image | awk -F\" '{print $2}') ; do lxc launch $f ; done
+for f in $(cat file.toml | grep -i image | awk -F\" '{print $2}') ; do lxc launch $f ; sleep 5 ; done
 for f in `lxc list -c n -f compact | tail -n +2` ; do lxc file push $HOME/bin/readme.lxc.md $f/tmp/ ; done
 sleep 10
 $HOME/bin/populate_hosts.sh
