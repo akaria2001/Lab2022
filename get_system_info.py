46059210744dfcac4a272957d16c3e4e803c180f
Amit Karia <amit_karia2001@hotmail.com>
2023-08-11 05:16:19 +0100

    Change to styles.css and adding get_system_info.py for troubleshooting

---
+++ get_system_info.py
@@ -0,0 +1,30 @@
+import platform
+import socket
+import re
+import uuid
+import psutil
+import logging
+
+
+def return_system_info():
+    try:
+        info={}
+        info['platform']=platform.system()
+        info['platform-release']=platform.release()
+        info['platform-version']=platform.version()
+        info['architecture']=platform.machine()
+        info['hostname']=socket.gethostname()
+        info['ip-address']=socket.gethostbyname(socket.gethostname())
+        info['system temp (c)']=int(psutil.sensors_temperatures()['k10temp'][0][1])
+        info['mac-address']=':'.join(re.findall('..', '%012x' % uuid.getnode()))
+        info['processor']=platform.processor()
+        info['ram']=str(round(psutil.virtual_memory().total / (1024.0 **3)))+" GB"
+        print(f"DEBUG : {type(info)}")
+        return info
+    except Exception as e:
+        logging.exception(e)
+
+
+
+if __name__ == '__main__':
+    print(return_system_info())
