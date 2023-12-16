5ea677ec28d767c8d5f125baef267e0b8e687864
Amit Karia <amit_karia2001@hotmail.com>
2023-12-16 12:26:29 +0000

    Minor updates

--- setup_host.sh
+++ setup_host.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+cd $HOME
 sudo apt-get install openssh-server -y
-sudo snap install neofetch ansible glances emacs-nox
+sudo apt-get install neofetch ansible glances emacs-nox -y
 sudo snap install lxd
 lxd init
 sudo apt install python3-pip and python3-virtualenv -y
@@ -10,7 +11,7 @@ virtualenv .
 source bin/activate
 pip3 install toml psutil flask
 sudo addgroup editors
-sudo adduser $USER
+sudo adduser $USER editors
 sudo chown :editors /etc/hosts
 sudo chmod 664 /etc/hosts
 sudo setfacl -m $USER:rw /etc/hosts
