0198c30e2f70032c431d337729b568ba7b7663d6
Amit Karia <amit_karia2001@hotmail.com>
2023-12-16 12:32:24 +0000

    Add microk8s lxd profile setup to setup_host.sh

--- setup_host.sh
+++ setup_host.sh
@@ -5,11 +5,13 @@ sudo apt-get install openssh-server -y
 sudo apt-get install neofetch ansible glances emacs-nox -y
 sudo snap install lxd
 lxd init
+wget https://raw.githubusercontent.com/ubuntu/microk8s/master/tests/lxc/microk8s.profile -O microk8s.profile
+cat microk8s.profile | lxc profile edit microk8s
 sudo apt install python3-pip and python3-virtualenv -y
 cd $HOME/bin
 virtualenv .
 source bin/activate
-pip3 install toml psutil flask
+pip3 install toml psutil flask ipdb
 sudo addgroup editors
 sudo adduser $USER editors
 sudo chown :editors /etc/hosts
