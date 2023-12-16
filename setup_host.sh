3e1043fd7e9fd293a79526a21309ffc10ec08d6a
Amit Karia <amit_karia2001@hotmail.com>
2023-12-16 12:47:21 +0000

    Further updates

--- setup_host.sh
+++ setup_host.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 cd $HOME
+sudo snap remove lxd --purge
 sudo apt-get install openssh-server -y
 sudo apt-get install neofetch ansible glances emacs-nox -y
 sudo snap install lxd
 lxd init
-wget https://raw.githubusercontent.com/ubuntu/microk8s/master/tests/lxc/microk8s.profile -O microk8s.profile
-cat microk8s.profile | lxc profile edit microk8s
+wget https://raw.githubusercontent.com/ubuntu/microk8s/master/tests/lxc/microk8s.profile -O $HOME/bin/microk8s.profile
+lxc profile create microk8s
+cat $HOME/bin/microk8s.profile | lxc profile edit microk8s
 sudo apt install python3-pip and python3-virtualenv -y
 cd $HOME/bin
 virtualenv .
