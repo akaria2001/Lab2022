2dd1c7182ad24c43ac8a25c21a96ad9135288f1d
Amit Karia <amit_karia2001@hotmail.com>
2023-12-21 11:27:24 +0000

    Automate ECDSA SSH key creation and add it to cloud-init configuration

--- setup_host.sh
+++ setup_host.sh
@@ -4,7 +4,7 @@ cd $HOME
 sudo apt-get update -y
 sudo snap remove lxd --purge
 sudo apt-get install openssh-server -y
-sudo apt-get install neofetch ansible glances vim emacs-nox acl -y
+sudo apt-get install neofetch ansible glances vim emacs-nox acl tmux -y
 sudo snap install lxd
 cat $HOME/bin/lxd_preseed.yaml | lxd init --preseed
 wget https://raw.githubusercontent.com/ubuntu/microk8s/master/tests/lxc/microk8s.profile -O $HOME/bin/microk8s.profile
@@ -21,10 +21,12 @@ sudo adduser $USER editors
 sudo chown :editors /etc/hosts
 sudo chmod 664 /etc/hosts
 sudo setfacl -m $USER:rw /etc/hosts
+rm -rfv $HOME/.ssh/id_ecdsa*
+ssh-keygen -b 256 -t ecdsa -q -f $HOME/.ssh/id_ecdsa -N ""
+cp cloud-init-test.yaml.template cloud-init-test.yaml
+sed -i "s|TMPKEY|$KEY|g" cloud-init-test.yaml
+clear
 printf "You can now create your MicroK8s cluster by running the following\n\n"
-printf "ssh-keygen -t rsa\n"
-printf "Enter the default options\n"
-printf "Copy the contens off $HOME/.ssh/id_rsa.pub to the cloud-init-test.yaml file line 15\n"
 printf "\n"
 printf "cd $HOME/bin\n"
 printf "source/bin/activate\n"
