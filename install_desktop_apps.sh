951bf20a9afc552dae14bb73a62568c7a7fdd446
Amit Karia <amit_karia2001@hotmail.com>
2023-12-22 13:30:39 +0000

    Add Install Desktop Snap Apps

---
+++ install_desktop_apps.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+sudo snap install 0ad
+sudo snap install android-studio
+sudo snap install code
+sudo snap install gitkraken
+sudo snap install intellij-idea-community
+sudo snap install obs-studio
+sudo snap install postman
+sudo snap install pycharm-community
+sudo snap install vlc
+sudo snap install warzone2100
+sudo snap install warzone2100-videos
