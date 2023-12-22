cba41dc3699bcdd4e9028270016fb720d7f0e6af
Amit Karia <amit_karia2001@hotmail.com>
2023-12-22 14:23:04 +0000

    Update desktop app install script

--- install_desktop_apps.sh
+++ install_desktop_apps.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-sudo snap install 0ad
-sudo snap install android-studio
-sudo snap install code
-sudo snap install gitkraken
-sudo snap install intellij-idea-community
+sudo snap install android-studio --classic
+sudo snap install code --classic
+sudo snap install gitkraken --classic
+sudo snap install intellij-idea-community --classic
 sudo snap install obs-studio
 sudo snap install postman
-sudo snap install pycharm-community
+sudo snap install pycharm-community --classic
 sudo snap install vlc
 sudo snap install warzone2100
 sudo snap install warzone2100-videos
