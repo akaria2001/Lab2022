170234382034ed625f5550534177af5cf5462fa8
Amit Karia <amit_karia2001@hotmail.com>
2025-07-29 07:45:37 +0100

    Create folder for new exports

--- export_lab.sh
+++ export_lab.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
-TIMESTAMP=`date "+%Y-%m-%d-%H:%M:%S"`
+# Create timestamp and folder
+TIMESTAMP=$(date "+%Y-%m-%d-%H-%M-%S")
+EXPORT_DIR="lxc-exports-$TIMESTAMP"
+mkdir -p "$EXPORT_DIR"
 
-for instance in $(lxc ls -c n -f csv) ; do lxc export $instance $instance-$TIMESTAMP-exported.tgz ; done
+# Loop through LXC containers and export each to the new folder
+for instance in $(lxc ls -c n -f csv); do
+    EXPORT_FILE="${EXPORT_DIR}/${instance}-${TIMESTAMP}-exported.tgz"
+    lxc export "$instance" "$EXPORT_FILE"
+    echo "Exported $instance to $EXPORT_FILE"
+done
+
