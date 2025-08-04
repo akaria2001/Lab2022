93733cbfd3504b2db3fd1fdd66ea359316b6c98d
Amit Karia <amit_karia2001@hotmail.com>
2025-08-04 14:26:44 +0100

    Update for Aug

---
+++ proxmox_new.sh
@@ -0,0 +1,2 @@
+ansible-playbook -v ansible-lab.yml -i ansible-proxmox32 -K -k -u akaria
+ansible-playbook -v update_os_proxmox_lab.yml -i ansible-proxmox32 -K -k -u akaria
---
+++ shutdown_proxmox_lab.sh
@@ -0,0 +1,1 @@
+ansible-playbook -v shutdown_proxmox_lab.yml -i ansible-proxmox32 -K -k -u akaria
